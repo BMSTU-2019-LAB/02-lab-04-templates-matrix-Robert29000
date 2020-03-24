a9facc819707319d3e985e90b447abb8a073579d
Robert29000 <33036695+Robert29000@users.noreply.github.com>
2020-03-24 21:03:44 +0300

    Update matrix.hpp

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -40,9 +40,9 @@ Matrix<T>::Matrix(int rows, int cols){
  this -> p = reinterpret_cast<T**>(malloc(rows * sizeof(T *)));
  this -> rows = rows;
  this -> cols = cols;
- for (size_t i = 0 ; i < rows ; i++){
-  p[i] = reinterpret_cast<T**>(malloc(cols * sizeof(T)));
-  for (size_t j = 0 ; j < cols ; j++){
+ for (int i = 0 ; i < rows ; i++){
+  p[i] = reinterpret_cast<T*>(malloc(cols * sizeof(T)));
+  for (int j = 0 ; j < cols ; j++){
    p[i][j] = 0;
   }
  }
@@ -53,9 +53,9 @@ Matrix<T>::Matrix(const Matrix& copy){
  cols = copy.Cols();
  rows = copy.Rows();
  this -> p = reinterpret_cast<T**>(malloc(rows * sizeof(T*)));
- for (size_t i = 0 ; i < copy.Rows() ; i++){
-  p[i] = reinterpret_cast<T**>(malloc(cols * sizeof(T)));
-  for (size_t j = 0 ; j < copy.Cols() ; j++){
+ for (int i = 0 ; i < copy.Rows() ; i++){
+  p[i] = reinterpret_cast<T*>(malloc(cols * sizeof(T)));
+  for (int j = 0 ; j < copy.Cols() ; j++){
    p[i][j] = copy[i][j];
   }
  }
@@ -70,8 +70,8 @@ template<class T>
 Matrix<T>& Matrix<T>::operator =(Matrix &m2){
  this->rows = m2.Rows();
  this->cols = m2.Cols();
- for (size_t i = 0 ; i < this->rows ; i++){
-  for (size_t j = 0 ; j < this->Cols() ; j++){
+ for (int i = 0 ; i < this->rows ; i++){
+  for (int j = 0 ; j < this->Cols() ; j++){
    this->p[i][j] = m2[i][j];
   }
  }
@@ -85,8 +85,8 @@ Matrix<T> Matrix<T>::operator +(Matrix& m2){
   return res;
  }
  Matrix<T> res(rows, cols);
- for (size_t i = 0; i < rows ; i++){
-  for (size_t j = 0 ; j < cols; j++){
+ for (int i = 0; i < rows ; i++){
+  for (int j = 0 ; j < cols; j++){
    res[i][j] = res[i][j] + m2[i][j];
   }
  }
@@ -100,8 +100,8 @@ Matrix<T> Matrix<T>::operator -(Matrix& m2){
   return res;
  }
  Matrix<T> res(rows, cols);
- for (size_t i = 0; i < rows ; i++){
-  for (size_t j = 0 ; j < cols; j++){
+ for (int i = 0; i < rows ; i++){
+  for (int j = 0 ; j < cols; j++){
    res[i][j] = (*this)[i][j] - m2[i][j];
   }
  }
@@ -115,9 +115,9 @@ Matrix<T> Matrix<T>::operator *(Matrix& m2){
   return res;
  }
  Matrix<T> res(rows, m2.Cols());
- for (size_t i = 0 ; i < res.Rows(); i++){
-  for (size_t j = 0 ; j < res.Cols(); j++){
-   for (size_t k = 0 ; k < cols ; k++){
+ for (int i = 0 ; i < res.Rows(); i++){
+  for (int j = 0 ; j < res.Cols(); j++){
+   for (int k = 0 ; k < cols ; k++){
     res[i][j] += (*this)[i][k] * m2[k][j];
    }
   }
@@ -130,9 +130,9 @@ Matrix<T> Matrix<T>::deleteRowsAndCols(Matrix<T> mat, int nRow, int nCol){
  Matrix<T> res(mat.Rows() - 1, mat.Cols() - 1);
  int numInRow = 0;
  int numInCol = 0;
- for (size_t i = 0 ; i < mat.Rows() ; i ++){
+ for (int i = 0 ; i < mat.Rows() ; i ++){
   if (i != nRow){
-   for (size_t j = 0 ; j < mat.Cols() ; j++){
+   for (int j = 0 ; j < mat.Cols() ; j++){
     if (j != nCol){
      res[numInRow][numInCol] = mat[i][j];
      numInCol += 1;
@@ -153,7 +153,7 @@ template<class T>
 double Matrix<T>::determinant(Matrix<T> mat){
  double det = 0;
  if (mat.Rows() > 2){
-  for (size_t i = 0 ; i < mat.Rows() ; i++){
+  for (int i = 0 ; i < mat.Rows() ; i++){
    det += pow(-1, i) * mat[0][i] * determinant(deleteRowsAndCols(mat, 0, i));
   }
  }else{
@@ -167,14 +167,14 @@ template<class T>
 Matrix<T> Matrix<T>::Inverse(){
  Matrix<T> inv(this->rows, this->cols);
  double det = determinant(*this);
- for (size_t i = 0; i < inv.Rows() ; i++){
-  for (size_t j = 0; j < inv.Cols() ; j++){
+ for (int i = 0; i < inv.Rows() ; i++){
+  for (int j = 0; j < inv.Cols() ; j++){
    inv[i][j] = pow(-1, i+j)*determinant(deleteRowsAndCols(*this, i, j));
   }
  }
  Matrix<T> invT(inv.Rows(), inv.Cols());
- for (size_t i = 0 ; i < invT.Rows(); i++){
-  for (size_t j = 0 ; j < invT.Cols(); j++){
+ for (int i = 0 ; i < invT.Rows(); i++){
+  for (int j = 0 ; j < invT.Cols(); j++){
    T temp = inv[j][i];
    T dividedTemp = temp / det;
    invT[i][j] = dividedTemp;
